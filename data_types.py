862c42ee33d811a59b5fa1e73f5830fb1a8dc40c
abindacarmo <abindacarmo08@gmail.com>
2026-03-11 21:00:37 +0900

    aumenta file esplikasaun basic konaba data types no oinsa ita bele check data types

---
+++ data_types.py
@@ -0,0 +1,23 @@
+# TIPO DE DADOS
+# iha python iha data types mak hanesan:
+
+
+# String mak data types ida ne'ebe utiliza hodi rai testu ka karakter
+naran = "Dito" # "Dito" ne'e hanesan string ida (str)
+
+# Integer mak data types ida ne'ebe utiliza hodi rai numero
+idade = 19 # 19 ne'e nudar integer (int)
+
+# Float mak data types ida ne'ebe utiliza hodi rai numero desimal
+altura = 176.0 # 176.0 ne'e nudar float (float)
+
+# Boolean mak data types ida ne'ebe rai value ne'ebe los no sala (True or False)
+dito_nudar_estudante = True # True ne'e nudar boolean (boolean)
+
+
+# OINSA ITA BELE CHECK TIPO DE DADOS
+# Utiliza fuction type() hodi bele check value ida ita bolu ne'e tama iha data type ida ne'ebe
+
+naran = 3.4
+
+print(type(naran))
