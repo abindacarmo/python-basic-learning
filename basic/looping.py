8589f6a5f2f14c64966e41823e1fa087acb6c5f8
abindacarmo <abindacarmo08@gmail.com>
2026-03-24 21:45:29 +0900

    update README and add file looping.py

---
+++ basic/looping.py
@@ -0,0 +1,30 @@
+# LOOPING
+
+# LOOP mak maneira ida ne'ebe haruka komputador hodi repete buat ruma dalabarak ho automatiku.
+
+# iha python loop fahe ba parte rua:
+# 1. for loop
+# 2. while loop
+
+#  exemplo FOR loop (utiliza hodi repete bazeia ba total espesifiku ka contain husi data ida(ex: list, string, nst))
+
+print("=====FOR LOOP=====")
+aifuan = ("apple", "orange", "mango", "berry")
+
+for i in aifuan: # ba variable i fo sai dados sira ne'ebe iha variable aifuan
+    print(i)
+
+# Output:
+# apple
+# orange
+# mango
+# berry
+
+print("=====WHILE LOOP=====")
+
+# Exemplo WHILE loop (utiliza hodi repete wainhira kondisaun ne'e ho valor ne'ebe True)
+
+i = 0 # variable i ho value 0
+while i < 5: # repete i wainhira i ne kiik liu duke 5
+    print(i)
+    i += 1 # aumeta ida kada loop
