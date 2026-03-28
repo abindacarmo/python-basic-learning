9c70342e270014aba13583c93b79556b09a85e72
abindacarmo <abindacarmo08@gmail.com>
2026-03-28 15:15:02 +0900

    update(basic) aumenta file treinu dictionary

---
+++ basic/treinu_dictionary.py
@@ -0,0 +1,11 @@
+# Dictionary - halo dictionary containt husi self data(naran, tinan, suco), hotu fosai dados hodu hotu
+
+self_data = {
+    "naran": "Abinda",
+    "tinan": 22,
+    "suco": "IB"
+}
+
+print("hau nia naran", self_data["naran"])
+print("hau nia tinan", self_data["tinan"])
+print("hau hela iha", self_data["suco"])
