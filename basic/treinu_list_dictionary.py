51a54440308c2ab1ce4cf35e5d9302a6dec8307c
abindacarmo <abindacarmo08@gmail.com>
2026-03-28 15:23:20 +0900

    update(basic) aumenta file treinu list_dictionary kombinasaun entre list ho dictionary

---
+++ basic/treinu_list_dictionary.py
@@ -0,0 +1,11 @@
+# Kombinasaun LIST-DICTIONARY - halo list of dictionary 
+
+estudante = [
+    {"naran": "Abinda", "tinan": 22},
+    {"naran": "Dito", "tinan": 20},
+    {"naran": "Maria", "tinan": 18}
+]
+
+index = 0
+for i in estudante:
+    print("Naran:",i["naran"],",", "Tinan:", i["tinan"])
